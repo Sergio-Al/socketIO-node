809073cc71c107cc45b5126d246f62ac2d53de66
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2022-03-22 16:26:44 -0400

    Creating a page with our socket
    To create a page and binding with our socket we did the following:
    - A new page is created in public/
    - Our new page index.html needs to have an instance of our socket. to fetch this instance we can use a embedded script https://cdnjs.com/libraries/socket.io from this page.
    - our socket instance needs to connect to a server, so we create a new script in our html page within there we connect our we create an listening instance with 'io' and a URL this URL needs to be the server socket.
    - Now we can listen for changes in our server listening the connection event (Remember the 'on' property is for events.
    - Now our socket server is working as expected.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // express server
-const app = require("express")();
+const express = require("express");
+const app = express();
 
 // socket server
 const server = require("http").createServer(app);
@@ -7,11 +8,14 @@ const server = require("http").createServer(app);
 // setting up socket server
 const io = require("socket.io")(server);
 
+// Serving the public directory
+app.use(express.static(__dirname + "/public"));
+
 // listening to events
 io.on("connection", () => {
-  /* */
+  console.log("a new client has arrived!");
 });
 
 server.listen(8080, () => {
-  console.log("app running on server 8080");
+  console.log("server running on server 8080");
 });
