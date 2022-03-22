7e3a207cf64dd716b5e21202e4e8afa7636b856d
Sergio-Al <54541807+Sergio-Al@users.noreply.github.com>
2022-03-22 15:49:59 -0400

    Basic socket io with express configuration

---
+++ index.js
@@ -0,0 +1,17 @@
+// express server
+const app = require("express")();
+
+// socket server
+const server = require("http").createServer(app);
+
+// setting up socket server
+const io = require("socket.io")(server);
+
+// listening to events
+io.on("connection", () => {
+  /* */
+});
+
+server.listen(8080, () => {
+  console.log("corriendo en puerto 8080");
+});
